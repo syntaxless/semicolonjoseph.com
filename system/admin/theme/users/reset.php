2db0a9fd2df58d6db4b5e947265d4c3d98cfcb29
Joseph <syntaxless@gmail.com>
2012-09-15 13:43:38 -0700

    No idea

--- system/admin/theme/users/reset.php
+++ system/admin/theme/users/reset.php
@@ -1,22 +1,25 @@
-<h1>Reset Password</h1>
+<h1><?php echo __('users.reset_password', 'Reset Password'); ?></h1>
 
 <?php echo Notifications::read(); ?>
 
 <section class="content">
 
-	<form method="post" action="<?php echo Url::current(); ?>" >
+	<form method="post" action="<?php echo Url::current(); ?>">
+
+		<input name="token" type="hidden" value="<?php echo Csrf::token(); ?>">
+		
 		<fieldset>
-			<legend>Password reset for <?php echo $user->real_name; ?></legend>
-			<em>Please enter a new password that you won&rsquo;t forget this time.</em>
+			<legend><?php echo __('users.reset_password_for', 'Password reset for '); ?> <?php echo $user->real_name; ?></legend>
+			<em><?php echo __('users.reset_password_explain', 'Please enter a new password that you won&rsquo;t forget this time.'); ?></em>
 			
 			<p>
-			    <label for="password">Password:</label>
+			    <label for="password"><?php echo __('users.password', 'Password'); ?>:</label>
 			    <input name="password" id="password" type="password" value="<?php echo Input::post('password'); ?>">
 			</p>
 
 			<p class="buttons">
-			    <button type="submit">Submit</button>
-			    <a href="<?php echo Url::make(); ?>">Back to <?php echo Config::get('metadata.sitename'); ?></a>
+			    <button type="submit"><?php echo __('users.submit', 'Submit'); ?></button>
+			    <a href="<?php echo Url::make(); ?>"><?php echo __('users.back_to', 'Back to '); ?> <?php echo Config::get('metadata.sitename'); ?></a>
 			</p>
 		</fieldset>
 	</form>
--- system/bootstrap.php
+++ system/bootstrap.php
@@ -1,36 +1,52 @@
 <?php defined('IN_CMS') or die('No direct access allowed.');
 
 /**
-	Check our environment
+	Include application helpers
 */
-if(version_compare(PHP_VERSION, '5.3.0', '<')) {
+require PATH . 'system/core/helpers.php';
+
+/**
+ *	Check our environment
+ */
+if(has_php(5.3) === false) {
 	// echo and exit with some usful information
 	echo 'Anchor requires PHP 5.3 or newer, your current environment is running PHP ' . PHP_VERSION;
 	exit(1);
 }
 
-// get our autoloader
-require PATH . 'system/classes/helpers.php';
-require PATH . 'system/classes/autoload.php';
+/**
+	Register Globals Fix
+*/
+if(ini_get('register_globals')) {
+	$globals = array($_REQUEST, $_SESSION, $_SERVER, $_FILES);
 
-// directly map classes for super fast loading
-Autoloader::map(array(
-	'Config' => PATH . 'system/classes/config.php',
-	'Error' => PATH . 'system/classes/error.php',
-	'Session' => PATH . 'system/classes/session.php',
-	'Anchor' => PATH . 'system/classes/anchor.php',
-	'Template' => PATH . 'system/classes/template.php',
-	'Request' => PATH . 'system/classes/request.php',
-	'Response' => PATH . 'system/classes/response.php',
-	'Log' => PATH . 'system/classes/log.php',
-	'Db' => PATH . 'system/classes/db.php',
-	'IoC' => PATH . 'system/classes/ioc.php',
-	'Url' => PATH . 'system/classes/url.php'
-));
+	foreach($globals as $global) {
+		foreach(array_keys($global) as $key) {
+			unset(${$key}); 
+		}
+	}
+}
+
+/**
+	Magic Quotes Fix
+	note: magic quotes is deprecated in PHP 5.3
+	src: php.net/manual/en/security.magicquotes.disabling.php
+*/
+if(magic_quotes()) {
+	$magics = array(&$_GET, &$_POST, &$_COOKIE, &$_REQUEST);
+
+	foreach($magics as &$magic) {
+		$magic = array_strip_slashes($magic);
+	}
+}
+
+// get our autoloader
+require PATH . 'system/core/autoload.php';
 
 // tell the autoloader where to find classes
 Autoloader::directory(array(
-	PATH . 'system/classes/'
+	PATH . 'system/core/',
+	PATH . 'system/library/'
 ));
 
 // register the auto loader
@@ -47,19 +63,6 @@ error_reporting(-1);
 ini_safe_set('display_errors', false);
 
 /**
-	Disable magic quotes
-	note: magic quotes is deprecated in PHP 5.3
-	src: php.net/manual/en/security.magicquotes.disabling.php
-*/
-if(function_exists('get_magic_quotes_gpc')) {
-	if(get_magic_quotes_gpc()) {
-		ini_safe_set('magic_quotes_gpc', false);
-		ini_safe_set('magic_quotes_runtime', false);
-		ini_safe_set('magic_quotes_sybase', false);
-	}
-}
-
-/**
 	Check our installation
 */
 if(Config::load(PATH . 'config.php') === false) {
@@ -70,6 +73,11 @@ if(Config::load(PATH . 'config.php') === false) {
 
 // Register the default timezone for the application.
 date_default_timezone_set(Config::get('application.timezone'));
+
+// set locale
+if(setlocale(LC_ALL, Config::get('application.language') . '.utf8') === false) {
+	Log::warning('setlocate failed, please check your system has ' . Config::get('application.language') . ' installed.');
+}
 
 // Register the PHP exception handler.
 set_exception_handler(array('Error', 'exception'));
